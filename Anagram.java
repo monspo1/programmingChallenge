6f260f301c183d5dda0b47f9ce39d6d145cc541b
monspo1 <monspo1@gmail.com>
2015-09-11 20:28:49 -0400

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -54,35 +54,35 @@ public class Solution {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
         
-		int t = scanner.nextInt();
-		while (scanner.hasNext()) {
-        //if(t < 1 || t > 10) System.out.println("Not proper testcases");  // <-- Don't include any unnecessary comments. It will NOT make pass some testcases.   
-
+        while (scanner.hasNext()) {
+        	//if(t < 1 || t > 10) System.out.println("Not proper testcases");  // <-- Don't include any unnecessary comments. It will NOT make pass some testcases.   
 			String s = scanner.next();  
 			int len = s.length();
-            
+			
 			if (len % 2 == 1) System.out.println( -1);
-			else{
+			else{	
 				int mid = len/2;
 				String s1 = s.substring(0, mid);
 				String s2 = s.substring(mid, len);
-				
 				int[] counts = new int[26];
 				
+				// For String s1
 				for (int i = 0; i < s1.length(); ++i) {
-				    char ch = Character.toLowerCase(s1.charAt(i));
-				    ++counts[ch - 'a'];
+					char ch = Character.toLowerCase(s1.charAt(i));
+					++counts[ch - 'a'];
 				}
 				
+				// For String s2
 				for (int i = 0; i < s2.length(); ++i) {
-				    char ch = Character.toLowerCase(s2.charAt(i));
-				    --counts[ch - 'a'];
+					char ch = Character.toLowerCase(s2.charAt(i));
+					--counts[ch - 'a'];
 				}
 
 				int cnt = 0;
 				for (int i = 0; i < counts.length; ++i) {
-				    cnt += Math.abs(counts[i]);
+					cnt += Math.abs(counts[i]);
 				}
 				System.out.println( cnt/2 );
 			} 
